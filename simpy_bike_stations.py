8aca756fe6c8cc44154e471d51deb8e8f599c0b0
imartinezl <inigomlap@gmail.com>
2020-01-10 12:49:03 +0100

    Stations properties + Walk, pull/push bike, select stations

--- simpy_bike_stations.py
+++ simpy_bike_stations.py
@@ -77,8 +77,11 @@ class Station:
         self.resource = simpy.Resource(self.env, capacity=1)
         self.container = simpy.Container(self.env, self.capacity, init=self.bikes)
 
-    def available(self):
+    def has_bikes(self):
         return self.container.level > 0
+
+    def has_slots(self):
+        return self.capacity - self.container.level > 0
 
     def empty(self):
         return self.container.level == 0
@@ -101,12 +104,14 @@ class Agent:
         self.checked_source_stations = []
         self.checked_target_stations = []
 
-        self.create_task()
-        self.select_source_station()
-        # print(self.source, self.target, self.source_ts, self.target_ts)
+        
         self.env.process(self.process())
 
     def process(self):
+        self.create_task()
+        self.select_source_station()
+        yield self.env.process(self.walk_to_source())
+        
         yield self.env.timeout(10)
         if self.print:
             print('Agent %d working at %.2f' % (self.agent_id, self.env.now))
@@ -127,9 +132,13 @@ class Agent:
         self.target_ts = self.source_ts + self.random_time()
 
         self.location = self.source
+        
 
-    def dist(self, station):
-        return np.linalg.norm(self.location - station.location)
+    def dist(self, a, b):
+        return np.linalg.norm(a - b)
+    
+    # def dist(self, station):
+    #     return np.linalg.norm(self.location - station.location)
 
     
 
@@ -137,22 +146,79 @@ class Agent:
         values = []
         for station in self.stations:
             station_id = station.station_id
-            available = station.available()
-            distance = self.dist(station)
+            has_bikes = station.has_bikes()
+            distance = self.dist(self.location, station.location)
             walkable = distance < WALK_RADIUS
-            visited = station_id in self.checked_source_stations
-            values.append((station_id, available, distance, walkable,visited))
-        labels = ['station_id','available','distance','walkable','visited']
-        types = [int,bool,float,bool,bool]
-        dtype = [('station_id',int),('available',bool),('distance',float),('walkable',bool),('visited',bool)]
-        self.a = np.array(values, dtype=list(zip(labels,types)))
-        self.a = np.sort(self.a, order='distance')
-        for e in self.a:
-            print(e, e.available)
-        #     if e.available and e.walkable and not e.visited:
-        #         self.source_station = self.stations[e.station_id]
-        #         print(self.source_station)
+            visited = station_id in self.checked_target_stations
+            values.append((station_id, has_bikes, distance, walkable, visited))
+        # labels = ['station_id','has_bikes','distance','walkable','visited']
+        # types = [int,bool,float,bool,bool]
+        # dtype = list(zip(labels, types))
+        dtype = [('station_id',int),('has_bikes',int),('distance',float),('walkable',int),('visited',int)]
+        self.source_station_info = np.array(values, dtype=dtype)
+        self.source_station_info_sorted = np.sort(self.source_station_info, order='distance')
+        for e in self.source_station_info_sorted:
+            print(e)
+            valid = e['has_bikes'] and e['walkable'] and not e['visited']
+            if valid:
+                station_id = e[station_id]
+                self.source_station = self.stations[station_id]
+                self.checked_source_stations.append(station_id)
+                break
+
                 
+    def walk_to_source(self):
+        if self.source_station is not None:
+            distance = self.dist(self.location, self.source_station.location)
+            yield self.env.timeout(distance)
+
+    def pull_bike(self):
+        if self.source_station.has_bikes():
+            yield self.source_station.container.get(1)
+            yield self.env.timeout(1)
+        else:
+            print("station has zero bikes")
+
+    def select_target_station(self):
+        values = []
+        for station in self.stations:
+            station_id = station.station_id
+            has_slots = station.has_slots()
+            distance = self.dist(self.location, station.location)
+            walkable = distance < WALK_RADIUS
+            visited = station_id in self.checked_target_stations
+            values.append((station_id, has_slots, distance, walkable,visited))
+        # labels = ['station_id','has_slots','distance','walkable','visited']
+        # types = [int,bool,float,bool,bool]
+        # dtype = list(zip(labels, types))
+        dtype = [('station_id',int),('has_slots',int),('distance',float),('walkable',int),('visited',int)]
+        self.target_station_info = np.array(values, dtype=dtype)
+        self.target_station_info_sorted = np.sort(self.target_station_info, order='distance')
+        for e in self.target_station_info_sorted:
+            print(e)
+            valid = e['has_slots'] and e['walkable'] and not e['visited']
+            if valid:
+                station_id = e[station_id]
+                self.target_station = self.stations[station_id]
+                self.checked_target_stations.append(station_id)
+                break
+
+    def ride_bike(self):
+        distance = self.dist(self.source_station.location, self.target_station.location)
+        yield self.env.timeout(distance)
+
+    def push_bike(self):
+        if self.target_station.has_slots():
+            yield self.target_station.container.get(1)
+            yield self.env.timeout(1)
+        else:
+            print("station has zero slots")
+    
+    def walk_to_target(self):
+        if self.target_station is not None:
+            distance = self.dist(self.target_station.location, self.location)
+            yield self.env.timeout(distance)
+
 
 
 
